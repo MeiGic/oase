43582069fadc589f043666cf0f2678addf601167
Ark <warren8890@gmail.com>
2014-05-06 02:57:02 +0800

    oase sql modify change log ------------------- Use database type: INNODB all table character set : utf8-unicode add the table function: foreign key ( i'm not sure there are nothing wrong) add table name prefix (oases) << i'm try the multiple replace on vim command line sql format double check the sql syntax the test on meigic seever is passed please everyone take a look thx; :wq

--- oase.sql
+++ oase.sql
@@ -1,8 +1,22 @@
+DROP DATABASE oase;
+
 CREATE DATABASE oase DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci; 
 
 USE oase; 
 
-CREATE TABLE employees 
+CREATE TABLE oase_unit 
+  ( 
+     u_id       INTEGER PRIMARY KEY, 
+     u_name     VARCHAR(100) NOT NULL, 
+     u_address  VARCHAR(1000) NOT NULL, 
+     type       VARCHAR(1000) NOT NULL, 
+     found_time DATE NOT NULL, 
+     manager    INTEGER NOT NULL 
+  ) 
+ENGINE = INNODB 
+CHARACTER SET = utf8; 
+
+CREATE TABLE oase_employee 
   ( 
      e_id      INTEGER PRIMARY KEY auto_increment, 
      type      VARCHAR(50) NOT NULL, 
@@ -12,53 +26,45 @@ CREATE TABLE employees
      address   VARCHAR(1000), 
      join_time DATE, 
      salary    INTEGER, 
-     u_id      INTEGER 
-  ); 
+     u_id      INTEGER, 
+     FOREIGN KEY(u_id) REFERENCES oase_unit(u_id) ON DELETE CASCADE 
+  ) 
+ENGINE = INNODB 
+CHARACTER SET = utf8; 
 
-CREATE TABLE unit 
+CREATE TABLE oase_work_log 
   ( 
-     u_id       INTEGER PRIMARY KEY, 
-     u_name     VARCHAR(100) NOT NULL, 
-     u_address  VARCHAR(1000) NOT NULL, 
-     type       INTEGER, 
-     found_time DATE NOT NULL, 
-     manager    INTEGER NOT NULL 
-  ); 
-
-CREATE TABLE work_log 
-  ( 
-     u_id       INTEGER PRIMARY KEY, 
+     wl_id      INTEGER PRIMARY KEY auto_increment, 
+     e_id       INTEGER, 
      date       DATE, 
      start_time TIME, 
-     stop_time  TIME 
-  ); 
+     stop_time  TIME, 
+     FOREIGN KEY(e_id) REFERENCES oase_employee(e_id) ON DELETE CASCADE 
+  ) 
+ENGINE = INNODB 
+CHARACTER SET = utf8; 
 
-CREATE TABLE payment 
+CREATE TABLE oase_payment 
   ( 
-     payment_id INTEGER auto_increment, 
+     payment_id INTEGER PRIMARY KEY auto_increment, 
      time_log   VARCHAR(10) NOT NULL, 
-     u_id       INTEGER NOT NULL, 
+     e_id       INTEGER NOT NULL, 
      work_log   INTEGER NOT NULL, 
      salary     INTEGER NOT NULL, 
-     PRIMARY KEY(payment_id) 
-  ); 
+     FOREIGN KEY(e_id) REFERENCES oase_employee(e_id) ON DELETE CASCADE 
+  ) 
+ENGINE = INNODB 
+CHARACTER SET = utf8; 
 
-CREATE TABLE order_deliver 
-  ( 
-     od_id          INTEGER auto_increment PRIMARY KEY, 
-     o_id           INTEGER NOT NULL, 
-     delivery_state VARCHAR(50) NOT NULL, 
-     delivery_time  DATETIME NOT NULL, 
-     delivery_man   INTEGER NOT NULL 
-  ); 
-
-CREATE TABLE product_cate 
+CREATE TABLE oase_product_cate 
   ( 
      pc_id   INTEGER auto_increment PRIMARY KEY, 
      pc_name VARCHAR(50) NOT NULL 
-  ); 
+  ) 
+ENGINE = INNODB 
+CHARACTER SET = utf8; 
 
-CREATE TABLE product 
+CREATE TABLE oase_product 
   ( 
      p_id    INTEGER PRIMARY KEY auto_increment, 
      p_name  VARCHAR(50) NOT NULL, 
@@ -68,80 +74,110 @@ CREATE TABLE product
      temp    INTEGER, 
      sweet   INTEGER, 
      offer   BOOLEAN NOT NULL, 
-     pc_id   INTEGER NOT NULL 
-  ); 
+     pc_id   INTEGER NOT NULL, 
+     FOREIGN KEY(pc_id) REFERENCES oase_product_cate(pc_id) ON DELETE CASCADE 
+  ) 
+ENGINE = INNODB 
+CHARACTER SET = utf8; 
 
-CREATE TABLE cookbook 
-  ( 
-     cb_id INTEGER PRIMARY KEY auto_increment, 
-     p_id  INTEGER NOT NULL, 
-     m_id  INTEGER NOT NULL, 
-     count INTEGER NOT NULL DEFAULT '1' 
-  ); 
-
-CREATE TABLE materials 
-  ( 
-     m_id   INTEGER PRIMARY KEY auto_increment, 
-     m_name VARCHAR(1000) NOT NULL, 
-     mc_id  INTEGER NOT NULL 
-  ); 
-
-CREATE TABLE materials_cate 
+CREATE TABLE oase_material_cate 
   ( 
      mc_id   INTEGER PRIMARY KEY auto_increment, 
      mc_name VARCHAR(100) NOT NULL 
-  ); 
+  ) 
+ENGINE = INNODB 
+CHARACTER SET = utf8; 
 
-CREATE TABLE materials_state 
+CREATE TABLE oase_material 
+  ( 
+     m_id   INTEGER PRIMARY KEY auto_increment, 
+     m_name VARCHAR(1000) NOT NULL, 
+     mc_id  INTEGER NOT NULL, 
+     FOREIGN KEY(mc_id) REFERENCES oase_material_cate(mc_id) ON DELETE CASCADE 
+  ) 
+ENGINE = INNODB 
+CHARACTER SET = utf8; 
+
+CREATE TABLE oase_material_state 
   ( 
      ms_id  INTEGER PRIMARY KEY auto_increment, 
      u_id   INTEGER NOT NULL, 
      m_id   INTEGER NOT NULL, 
      remain INTEGER DEFAULT '0' 
-  ); 
+  ) 
+ENGINE = INNODB 
+CHARACTER SET = utf8; 
 
-CREATE TABLE material_order_log 
+CREATE TABLE oase_material_order 
   ( 
      mol_id      INTEGER PRIMARY KEY auto_increment, 
      timelog     TIMESTAMP, 
      store_id    INTEGER NOT NULL, 
      supplier_id INTEGER NOT NULL, 
      state       VARCHAR(1000) NOT NULL, 
-     stuff       VARCHAR(1000) NOT NULL 
-  ); 
+     stuff       VARCHAR(1000) NOT NULL, 
+     FOREIGN KEY(store_id) REFERENCES oase_unit(u_id) ON DELETE CASCADE, 
+     FOREIGN KEY(supplier_id) REFERENCES oase_unit(u_id) ON DELETE CASCADE 
+  ) 
+ENGINE = INNODB 
+CHARACTER SET = utf8; 
 
-CREATE TABLE material_order_deliver 
+CREATE TABLE oase_material_order_deliver 
   ( 
      mod_id        INTEGER PRIMARY KEY auto_increment, 
      mol_id        INTEGER NOT NULL, 
      deliver_state VARCHAR(1000) NOT NULL, 
      delivery_man  INTEGER NOT NULL, 
-     timelog       TIMESTAMP 
-  ); 
+     timelog       TIMESTAMP, 
+     FOREIGN KEY(mol_id) REFERENCES oase_material_order(mol_id) ON DELETE 
+     CASCADE 
+  ) 
+ENGINE = INNODB 
+CHARACTER SET = utf8; 
 
-CREATE TABLE customer_style 
+CREATE TABLE oase_cookbook 
   ( 
-     cs_id   INTEGER PRIMARY KEY auto_increment, 
-     cs_name VARCHAR(1000) NOT NULL, 
-     csc_id  INTEGER NOT NULL DEFAULT '1' 
-  ); 
+     cb_id INTEGER PRIMARY KEY auto_increment, 
+     p_id  INTEGER NOT NULL, 
+     m_id  INTEGER NOT NULL, 
+     count INTEGER NOT NULL DEFAULT '1', 
+     FOREIGN KEY(p_id) REFERENCES oase_product(p_id) ON DELETE CASCADE, 
+     FOREIGN KEY(m_id) REFERENCES oase_material(m_id) ON DELETE CASCADE 
+  ) 
+ENGINE = INNODB 
+CHARACTER SET = utf8; 
 
-CREATE TABLE custormer_style_cate 
-  ( 
-     csc_id   INTEGER auto_increment PRIMARY KEY, 
-     csc_name VARCHAR(1000) NOT NULL 
-  ); 
-
-CREATE TABLE special_order 
+CREATE TABLE oase_special_order 
   ( 
      so_id   INTEGER PRIMARY KEY auto_increment, 
      so_name VARCHAR(1000) NOT NULL, 
      price   INTEGER NOT NULL DEFAULT '1', 
      m_id    INTEGER NOT NULL, 
      count   INTEGER NOT NULL DEFAULT '1' 
-  ); 
+  ) 
+ENGINE = INNODB 
+CHARACTER SET = utf8; 
 
-CREATE TABLE orders 
+CREATE TABLE oase_custormer_style_cate 
+  ( 
+     csc_id   INTEGER auto_increment PRIMARY KEY, 
+     csc_name VARCHAR(1000) NOT NULL 
+  ) 
+ENGINE = INNODB 
+CHARACTER SET = utf8; 
+
+CREATE TABLE oase_customer_style 
+  ( 
+     cs_id   INTEGER PRIMARY KEY auto_increment, 
+     cs_name VARCHAR(1000) NOT NULL, 
+     csc_id  INTEGER NOT NULL DEFAULT '1', 
+     FOREIGN KEY(csc_id) REFERENCES oase_custormer_style_cate(csc_id) ON DELETE 
+     CASCADE 
+  ) 
+ENGINE = INNODB 
+CHARACTER SET = utf8; 
+
+CREATE TABLE oase_order 
   ( 
      o_id            INTEGER PRIMARY KEY auto_increment, 
      timelog         TIMESTAMP, 
@@ -150,20 +186,42 @@ CREATE TABLE orders
      deliver_request BOOLEAN DEFAULT '0', 
      state           VARCHAR(100), 
      mem_id          VARCHAR(500), 
-     u_id            INTEGER NOT NULL 
-  ); 
+     u_id            INTEGER NOT NULL, 
+     FOREIGN KEY(u_id) REFERENCES oase_unit(u_id) ON DELETE CASCADE 
+  ) 
+ENGINE = INNODB 
+CHARACTER SET = utf8; 
 
-CREATE TABLE order_detail 
+CREATE TABLE oase_order_detail 
   ( 
      od_id INTEGER PRIMARY KEY, 
      o_id  INTEGER NOT NULL, 
      p_id  INTEGER NOT NULL, 
      count INTEGER NOT NULL DEFAULT '1', 
      so_id INTEGER, 
-     cs_id INTEGER 
-  ); 
+     cs_id INTEGER, 
+     FOREIGN KEY(o_id) REFERENCES oase_order(o_id) ON DELETE CASCADE, 
+     FOREIGN KEY(p_id) REFERENCES oase_product(p_id) ON DELETE CASCADE, 
+     FOREIGN KEY(so_id) REFERENCES oase_special_order(so_id) ON DELETE CASCADE, 
+     FOREIGN KEY(cs_id) REFERENCES oase_customer_style(cs_id) ON DELETE CASCADE 
+  ) 
+ENGINE = INNODB 
+CHARACTER SET = utf8; 
 
-CREATE TABLE member 
+CREATE TABLE oase_order_deliver 
+  ( 
+     od_id          INTEGER PRIMARY KEY auto_increment, 
+     o_id           INTEGER NOT NULL, 
+     delivery_state VARCHAR(50) NOT NULL, 
+     delivery_time  DATETIME NOT NULL, 
+     delivery_man   INTEGER NOT NULL, 
+     FOREIGN KEY(o_id) REFERENCES oase_order(o_id) ON DELETE CASCADE 
+  ) 
+ENGINE = INNODB 
+CHARACTER SET = utf8; 
+
+
+CREATE TABLE oase_member 
   ( 
      mem_id       INTEGER PRIMARY KEY auto_increment, 
      jointime     DATE NOT NULL, 
@@ -171,12 +229,16 @@ CREATE TABLE member
      mem_address  VARCHAR (100000), 
      carrer       VARCHAR(1000), 
      mem_birthday DATE NOT NULL 
-  ); 
+  ) 
+ENGINE = INNODB 
+CHARACTER SET = utf8; 
 
-CREATE TABLE web_dashboard 
+CREATE TABLE oase_web_dashboard 
   ( 
      wd_id   INTEGER PRIMARY KEY auto_increment, 
      timelog TIMESTAMP, 
      title   VARCHAR(1000) NOT NULL, 
      content VARCHAR(100000) 
-  ); 
+  ) 
+ENGINE = INNODB 
+CHARACTER SET = utf8; 

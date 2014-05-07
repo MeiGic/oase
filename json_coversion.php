7e334a0b5c61c9018d8b4b86efa739df9fabd76b
longbiau <longbiau@hotmail.com.tw>
2014-05-07 13:34:05 +0800

    Update: Use ?type request as parameter.

--- json_coversion.php
+++ json_coversion.php
@@ -1,27 +1,41 @@
 <?php
 /************************************************************************\
 |* Parameter:                                                           *|
-|*   $table => the name of the table.                                   *|
+|*   ?type: The target table.                                           *|
 |* Return Value:                                                        *|
-|*   JSON encode which is convert from the imformation of the table.    *|
+|*   JSON encode which is convert from the information of the table.    *|
 |* Exception:                                                           *|
 |* 1. Error (if SQL connection fail).                                   *|
-|* 2. Table is not exist (if the table isn't exist).                    *|    
-|* By longbiau, Writen on 2014/05/06.                                   *|  
+|* 2. Table is not exist (if the table isn't exist).                    *|
+|* By longbiau, Writen on 2014/05/07.                                   *|
 \************************************************************************/
+
+$type = $_REQUEST['type'];
+switch($type){
+	case 'productlist':
+		echo get_json_from_table('oase_product');
+		break;
+	case 'storelist':
+		echo get_json_from_table('oase_unit');
+		break;
+	default:
+		echo 'The parameter is not accepted!<br>';
+		break;
+}
 function get_json_from_table($table){
-	$link = mysql_connect("127.0.0.1" , "root" , "#password") or die("Error " . mysql_error($link));
-	mysql_query("SET names 'utf8'");
-	mysql_select_db('oase' , $link);
+	$link = mysqli_connect("127.0.0.1" , "root" , "#password") or die("Error " . mysqli_error($link));
+	mysqli_select_db($link, "oase") or die ("no database"); 
+	$link->set_charset('utf8');
 	$sql1 = "select count(*) from ".$table;
-	$query1 = mysql_query($sql1 , $link);
+	$query1 = mysqli_query($link, $sql1);
 	if($query1 == 0)
 		die("Table is not exist!");
 	$sql = "SELECT * from ".$table;
-	$query = mysql_query($sql , $link);
+	$query = mysqli_query($link, $sql);
 	$json = array();
-	for($i=0; $row = mysql_fetch_array($query , MYSQL_ASSOC); $i++)
+	for($i=0; $row = mysqli_fetch_assoc($query); $i++)
 		$json[$i] = $row;
+	mysqli_close($link);
 	return json_encode($json);
 }
 ?>

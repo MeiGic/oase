9cfd19ded86aca11e80f43d6757ae4242b65acd5
Cheng-Han Wu <jacky_cute0808@hotmail.com>
2014-06-19 02:29:35 +0800

    Add oase delivery app.

---
+++ oase-delivery-app/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,9 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.mirasense.scanditsdk.plugin": "1.1.0"
+}
+// BOTTOM OF METADATA
+});
